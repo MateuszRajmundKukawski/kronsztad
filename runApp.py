39c5ce0ea322e064b8a9b6bf58279c4ba49aeb83
MateuszRajmundKukawski <mrkukawski@gmail.com>
2015-05-12 22:17:41 +0200

    first commit

---
+++ runApp.py
@@ -0,0 +1,99 @@
+#-*- coding: windows-1250 -*-
+import glob
+import os
+
+
+lista_plikow = glob.glob('*.txt') #modu³ glob.glob tworzy listê pliów, w nawiassach podajemy œcie¿kê
+                                # tu wystarczy '*.txt' - lista wszystkich plików txt w folderze,
+                                # w którym jest program
+if lista_plikow == []: # sprawdza czy s¹ pliki, jeœli nie to lista_plikow jest pusta
+    raw_input("Nie ma plików")
+    exit()
+else:
+    print 'Lista plików w folderze:' # wyœwietla w konsoli tekst 'lista.. bala bla bal'
+    licznik=0
+    for plik in lista_plikow: #pêtla for -> dla ka¿dego emelentu (tu pliku) z listy (lista_plikow) zrób:
+        licznik +=1             # licznik+1(bo indeks listy jest od 0)
+        print licznik, plik # wyœwietl licznik oraz nazwê pliku
+
+index_nawy_pliku =  raw_input('Aby zakonczyc wpisz: koniec\nlub podaj numer pliku: ')
+
+while True:
+    if index_nawy_pliku == 'koniec':
+        raw_input('Koniec, dowolny klawisz aby zakoñczyæ')
+        exit()
+
+    try:
+        index_nawy_pliku = int(index_nawy_pliku)-1
+        if index_nawy_pliku == -1: # w³asny wyj¹tek, bo gdy index == -1 to odwo³uje siê do ostatniego elementu listy
+            raise
+        nazwa_pliku = lista_plikow[index_nawy_pliku]
+        print "Wybrano plik:", nazwa_pliku
+        break # przerywa dzia³anie pêtli
+    except:
+        print 'Podano z³¹ wartoœæ'
+        index_nawy_pliku =  raw_input('Podaj numer pliku: ')
+
+
+korekta = raw_input('Aby zakonczyc wpisz: koniec\nlub podaj wartoœæ: ')
+while True:
+    if korekta == 'koniec':
+        raw_input('Koniec, dowolny klawisz aby zakoñczyæ')
+        exit()
+    try:
+        korekta = float(korekta)
+        print "Wybrano wartosc:", korekta
+        break
+    except:
+        print 'Podano z³¹ wartoœæ'
+        korekta = raw_input('Podaj poprawn¹ wartoœæ: ')
+
+
+plik_wyjsciowy = raw_input('Podaj nazwê pliku wyjœciowego\n[enter]\t->\t domyœlna nazwa:\twynikowy.txt\n<nazwa bez rozszerzenia>: ')
+if plik_wyjsciowy == '':
+    plik_wyjsciowy = 'wynik.txt'
+elif plik_wyjsciowy == 'koniec':
+    raw_input('Koniec, dowolny klawisz aby zakoñczyæ')
+else:
+    plik_wyjsciowy = plik_wyjsciowy+'.txt'
+
+
+try:
+    os.system('clear')
+    os.system('cls')
+except:
+    print '\n'*50
+
+print '\n'*50
+
+
+
+
+
+print "Wybrano plik:", nazwa_pliku
+print "Wybrano wartosc:", korekta
+print "Wybrano plik wyjœciowy:", plik_wyjsciowy
+
+plik_zrodlowy = open(nazwa_pliku) #mo¿na tak pracowaæ z plikiem, tylko trzeba pamiêtaæ zamkn¹æ
+with open(plik_wyjsciowy, 'w') as f: # lub tak (wtedy z plik automatycznie zostanie zamkniêty)
+    for linia in plik_zrodlowy:
+        linia = linia.rstrip('\n') # usuwa znak koñca linii
+        linia = linia.split(';') # dzieli linie na listê, separatorem jest ;
+        id_wartosci = linia[0] #pobiera element z 1. kolumny (index 0)
+        wartosc = linia[1] #pobiera element z 2. kolumny (index 1)
+        przelicz = linia[2] #pobiera element z 3. kolumny (index 2)
+        if przelicz == 'K': # jesli 'K' w 3 kolumnie to:
+            nowa_wartosc = float(wartosc) + korekta #float(x) zamienia tekst (string) na liczbe zmienno przecinkow¹
+            nowa_wartosc = str(nowa_wartosc)
+        else:
+                nowa_wartosc = wartosc
+
+        print id_wartosci, wartosc, nowa_wartosc, przelicz
+        f.write(id_wartosci+';'+nowa_wartosc+';'+przelicz+'\n')
+
+plik_zrodlowy.close()
+
+
+
+
+
